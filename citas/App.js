e25a0e99d5682c73bdcf22ea4f49925e34faa3ff
unknown <fmendoza.ospina7@outlook.com>
2022-10-28 14:09:32 -0500

    React-Native

--- citas/App.js
+++ citas/App.js
@@ -7,13 +7,62 @@ import {
   Text,
   useColorScheme,
   View,
+  Button,
+  Pressable
 } from 'react-native';
 
 const App = () => {
+
+  const nuevaCitaHandler = () => {
+
+  }
   
   return (
-    <Text>Hola</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titulo}>
+        Administrador de Citas {''}
+        <Text style={styles.tituloBold}>Veterinaria</Text>
+      </Text>
+
+      <Pressable 
+        onPress={nuevaCitaHandler}
+        style={styles.btnNuevaCita}
+      >
+        <Text style={styles.btnTextoNuevaCita}>Nueva Cita</Text>
+      </Pressable>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F3F4F6',
+    flex: 1
+  },
+  titulo: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
+    fontWeight: '600'
+  },
+  tituloBold: {
+    fontWeight: '900',
+    color: '#6D28D9'
+  },
+  btnNuevaCita: {
+    backgroundColor: '#6D28D9',
+    padding: 15,
+    marginTop: 20,
+    marginHorizontal: 20,
+    borderRadius: 10    
+  },
+  btnTextoNuevaCita: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: '900',
+    textTransform: 'uppercase'
+  }
+})
 
 export default App;
